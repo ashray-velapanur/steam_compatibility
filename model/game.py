9d13b5c2fdda0e3b7d05d03ec05c1421a24d890f
ashray-velapanur <ashray.velapanur@gmail.com>
2015-04-04 17:43:13 +0530

    added genres property to games

--- model/game.py
+++ model/game.py
@@ -1,5 +1,11 @@
 from google.appengine.ext import db
 
+from model.genre import genres
+
 class Game(db.Model):
     name = db.StringProperty(indexed=False)
     tags = db.StringListProperty(indexed=False)
+
+    @property
+    def genres(self):
+        return [tag for tag in self.tags if tag in genres]
